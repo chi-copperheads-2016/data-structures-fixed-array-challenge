2721a84d031efc481dc51e531ed12940dd300a61
Noah Heinrich <noahheinrich@gmail.com>
2016-01-25 15:08:24 -0600

    Add new method, get method

--- source/fixed_array.rb
+++ source/fixed_array.rb
@@ -1,3 +1,17 @@
 class FixedArray
+  attr_reader :length
+  attr_accessor :array
+  def initialize(size)
+    @length = size
+    @array = Array.new(@length)
+  end
+
+  def get(index)
+    if index < @length && index >= 0
+      @array[index]
+    else
+      raise OutOfBoundsException
+    end
+  end
 
 end
--- source/fixed_array_spec.rb
+++ source/fixed_array_spec.rb
@@ -1,5 +1,22 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
+  before(:all) do
+    @fixed_array = FixedArray.new(5)
+  end
+
+  describe "#new" do
+    it "creates a fixed array of the passed size" do
+      expect(@fixed_array.length).to eq 5
+    end
+  end
+
+  describe "#get" do
+    it "returns the value at the given index" do
+      expect(@fixed_array.get(0)).to be nil
+    end
+    it "raises an exception if passed an index not included in the array" do
+      expect(@fixed_array.get(10)).to raise_error
+    end
+  end
 end
