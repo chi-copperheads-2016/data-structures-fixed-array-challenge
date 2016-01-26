91f6df5c41fd318983403b9c735aa3d0a590888e
Farman Pirzada <farmanp.home@gmai.com>
2016-01-26 00:24:05 -0500

    Create initial solution for fixed array

--- source/fixed_array.rb
+++ source/fixed_array.rb
@@ -1,3 +1,21 @@
 class FixedArray
+  attr_accessor :size, :index, :element, :array
 
+  def initialize(size, array)
+    @size = size
+    @array = Array.new(size, array)
+  end
+
+  def get(index)
+    if index <= self.size
+      self.array[index]
+    else
+      raise "Index does not exist"
+    end
+  end
+
+  def set(index, element)
+    self.get(index) << element
+    element
+  end
 end
