7c37231a0b7a96d507003edcaa54be6bda4d41f8
frizzbolt <joshua.a.abrams@gmail.com>
2016-01-25 20:51:31 -0600

    Add fixed array challenge files

--- source/fixed_array.rb
+++ source/fixed_array.rb
@@ -1,3 +1,29 @@
 class FixedArray
 
+  attr_reader :array
+
+  def initialize(size)
+    @array = Array.new(size)
+  end
+
+  def is_within_bounds(index)
+    index <= @array.length && index > 0
+  end
+
+  def get(index)
+    if is_within_bounds(index)
+      @array[index]
+    else
+      raise Exception.new("Out of Bounds")
+    end
+  end
+
+  def set(index, element)
+    if is_within_bounds(index)
+      @array[index] = element
+    else
+      raise Exception.new("Out of Bounds")
+    end
+  end
+
 end
